769d45d990b2abd5830106743c6aec34c5498a87
강성일 <gangseong-il@gangseong-il-ui-MacBook-Pro.local>
2016-09-27 18:37:00 +0900

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+function NetworkError(message) {
+    this.name = 'NetworkError';
+    this.message = message;
+    this.stack = (new Error()).stack;
+}
+
+NetworkError.prototype = new Error;
+
+
+function timeoutPromise(timeout, promise) {
+    var wrapPromise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            reject(new NetworkError('Promise not resolved after ' + timeout + ' ms'));
+        }, timeout);
+    });
+
+    return Promise.race([promise, wrapPromise]);
+}
+
+module.exports = {
+    timeoutPromise: timeoutPromise,
+    NetworkError: NetworkError
+};
